388a0d2a4a4b70fd975419d4595385e1c2977e70
caden99 <123427611+caden99@users.noreply.github.com>
2023-03-12 10:08:07 +0000

    Test

--- src/question_4/question4.h
+++ src/question_4/question4.h
@@ -1,2 +1,7 @@
 //
 bool test_config();
+#include <iostream>
+using std::string;
+
+string get_fib_number(int input);
+int run_fib_number();
--- test/question_test_4/question_tests_4.cpp
+++ test/question_test_4/question_tests_4.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "question4.h"
+using namespace std;
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -9,4 +10,11 @@ TEST_CASE("Verify Test Configuration", "verification") {
 TEST_CASE("test")
 {
 	REQUIRE(test_config() == true);
+}
+TEST_CASE("Test for sequence")
+{
+	REQUIRE(get_fib_number(5) == "0 1 1 2 3 5"); // Code matches the test case 
+	REQUIRE(get_fib_number(7) == "0 1 1 2 3 5 8");
+	REQUIRE(get_fib_number(10) == "0 1 1 2 3 5 8 13 21 34 55");
+	REQUIRE(get_fib_number(12) == "0 1 1 2 3 5 8 13 21 34 55 89 144");
 }
