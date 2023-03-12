bb939d83b050381ede6b378db1bfb55bcb7ce8cf
caden99 <123427611+caden99@users.noreply.github.com>
2023-03-12 18:39:57 +0000

    Test

--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -1,6 +1,27 @@
 #include "question2.h"
-
+#include <iostream>
+using std::cout; using std::cin;
 bool test_config()
 {
     return true;
+}
+
+
+
+int find_gcd(int a, int b) {
+  while (a != 0 && b != 0) {
+      if (a > b) {
+          a %= b;
+      } else {
+          b %= a;
+      }
+  }
+  if (a==0){
+    return b;
+    
+  }
+  else{
+    return a;
+    
+  }
 }
--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -4,3 +4,8 @@ bool test_config()
 {
     return true;
 }
+
+double get_farenheit(double c){
+  double farenheit = (1.8 * c)+32;
+  return farenheit;
+}
